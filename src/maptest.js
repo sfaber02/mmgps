a90a1231d5ebea17534130f5dcc13147bae5fb53
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-04-24 16:02:34 -0400

    got other google maps library working

--- src/maptest.js
+++ src/maptest.js
@@ -1,36 +1,42 @@
 import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
-import { GoogleMap } from 'react-google-maps';
-import { WrappedMap } from './map';
 import './styles/map.css';
 
+import { GoogleMap, useLoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 
-
+const GOOGLEMAPSKEY = process.env.REACT_APP_GOOGLE_MAPS_KEY;
+const libraries = ['places'];
+const mapContainerStyle = {
+    width: '100vw',
+    height: '100vh',
+}
+const center = {
+    lat: 40.777955,
+    lng: -73.968740,
+}
 
 const TheMap = () => {
-
     const navigate = useNavigate();
+    const {isLoaded, loadError} = useLoadScript({
+        googleMapsApiKey: GOOGLEMAPSKEY,
+        libraries,
+    })
+
+    if (loadError) return 'error loading maps'
+    if (!isLoaded) return 'LOADING'
 
     const changePath = () => navigate('two');
     const changePath2 = () => navigate('/');
 
-    const GOOGLEMAPSKEY = process.env.REACT_APP_GOOGLE_MAPS_KEY;
 
     const Path1 = () => {
        
-        return (
-            <div id='mapContainer'>
-                <WrappedMap 
-                    googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${GOOGLEMAPSKEY}`} 
-                    loadingElement={<div style={{height: '100%'}}/>}
-                    containerElement={<div style={{height: '100%'}}/>}
-                    mapElement={<div style={{height: '100%'}}/>}
-                />
-            </div>
-
-        )
-        
-        
+            return (
+                <div>
+                    <GoogleMap mapContainerStyle={mapContainerStyle} zoom={15} center={center} ></ GoogleMap>
+                </div>
+            )
+  
     }
 
     const Path2 = () => {
