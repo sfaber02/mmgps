6b44e52c6e206a46f65c1e7e144bb5f3aea5c662
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-09-18 16:54:49 -0400

    started work on conversion algorithm

--- src/components/Explorer-SubComponents/Canvas.js
+++ src/components/Explorer-SubComponents/Canvas.js
@@ -1,22 +1,41 @@
 import React, { useRef, useEffect } from "react";
 
-export const Canvas = () => {
+export const Canvas = ({ coords }) => {
+    console.log('canvas coords', coords);
     const canvasRef = useRef(null); // ref to hold canvas element
 
     const draw = (ctx) => {
-        ctx.fillStyle = "#000000";
-        ctx.beginPath();
-
-        //ctx.arc(50, 100, 20, 0, 2 * Math.PI);
-        //ctx.fill();
+        // ctx.fillStyle = "#FFFFFF";
+        // ctx.beginPath();
+        // console.log("DRAW");
+        // let start = {
+        //     x: (coords[0].lng * 500) / 360,
+        //     y: (coords[0].lat * 500) / 180,
+        // };
+        // console.log('start', start);
+        // ctx.moveTo(start.x, start.y);
+        // for (let i = 1; i < coords.length; i++) {
+        //     let coord = {
+        //         x: (coords[i].lng * 500) / 360,
+        //         y: (coords[i].lat * 500) / 180,
+        //     };
+        //     console.log("coord", coord);
+        //     ctx.lineTo(coord.x, coord.y);
+        // }
+        // ctx.lineTo(start.x, start.y);
+        ctx.lineTo(300, 300);
+        ctx.lineTo(150, 150);
+        ctx.stroke();
     };
 
     useEffect(() => {
-        const canvas = canvasRef.current; // canvas element
-        const context = canvas.getContext("2d"); // 2d canvas context
+        if (coords.length > 1) {
+            const canvas = canvasRef.current; // canvas element
+            const context = canvas.getContext("2d"); // 2d canvas context
 
-        draw(context);
-    }, [draw]);
+            draw(context);
+        }
+    }, [draw, coords]);
 
-    return <canvas className="explorer-canvas" ref={canvasRef} />;
+    return <canvas className="explorer-canvas" width="500" height="500" ref={canvasRef} />;
 };
--- src/components/Explorer.js
+++ src/components/Explorer.js
@@ -5,17 +5,17 @@ import { Canvas } from "./Explorer-SubComponents/Canvas";
 import "../styles/Explorer/explorer.scss";
 
 const Explorer = ({ polygons }) => {
-    const [coords, setCoords] = useState({});
-    console.log(polygons);
-
+    const [coords, setCoords] = useState([]);
+        
     useEffect(() => {
         if (polygons) {
+            
             let path = polygons.getPath();
             let bounds = [];
             for (let i = 0; i < path.length; i++) {
                 bounds.push({
-                    lat: path.getAt(i).lat(),
-                    lng: path.getAt(i).lng(),
+                    lat: path.getAt(i).lat() + 90, // convert coords to only postive nums
+                    lng: path.getAt(i).lng() + 180, // convert coords to only positive
                 });
             }
             setCoords(bounds);
@@ -23,27 +23,33 @@ const Explorer = ({ polygons }) => {
     }, [polygons]);
 
     
-    // determine total dimensions of shape by subtracting min from max
+   
     // convert these into ratios to draw shape
 
     useEffect(() => {
         // get max/min lat and lng
         if (coords.length > 1) {
-            let minMax = {
+            const minMax = {
                 lat: { min: Infinity, max: -Infinity },
                 lng: { min: Infinity, max: -Infinity },
             };
             for (let coord of coords) {
-                console.log(coord);
-                if (coord.lat > minMax.lat.max) minMax.lat.max = coord.lat;
-                if (coord.lat < minMax.lat.min) minMax.lat.min = coord.lat;
-                if (coord.lng > minMax.lng.max) minMax.lng.max = coord.lng;
-                if (coord.lng < minMax.lng.min) minMax.lng.min = coord.lng;
+                // console.log(coord);
+                if (coord.lat > minMax.lat.max)
+                    minMax.lat.max = coord.lat;
+                if (coord.lat < minMax.lat.min)
+                    minMax.lat.min = coord.lat;
+                if (coord.lng > minMax.lng.max)
+                    minMax.lng.max = coord.lng;
+                if (coord.lng < minMax.lng.min)
+                    minMax.lng.min = coord.lng;
             }
-            console.log(minMax);
+            console.log('minMax', minMax);
 
-            
-
+            // determine total dimensions of shape by subtracting min from max
+            const width = minMax.lng.max - minMax.lng.min;
+            const height = minMax.lat.max - minMax.lat.min;
+            console.log(width, height);
 
 
         }
@@ -56,7 +62,7 @@ const Explorer = ({ polygons }) => {
             ) : (
                 <p>No Polygons</p>
             )}
-            <Canvas />
+            <Canvas coords={coords} />
         </>
     );
 };
