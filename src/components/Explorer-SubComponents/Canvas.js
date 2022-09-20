6487e2a3926c295624565cdaee0462bfddaa2444
Shawn Faber <96348445+sfaber02@users.noreply.github.com>
2022-09-20 18:27:25 -0400

    started work on static map generation

--- src/components/Explorer-SubComponents/Canvas.js
+++ src/components/Explorer-SubComponents/Canvas.js
@@ -1,44 +1,8 @@
 import React, { useRef, useEffect, useState } from "react";
 
-export const Canvas = ({ coords }) => {
+export const Canvas = ({ coords, minMax, multiplier }) => {
     console.log('canvas coords', coords);
     const canvasRef = useRef(null); // ref to hold canvas element
-
-    const [multiplier, setMultiplier] = useState(0);
-    const [minMax, setMinMax] = useState({
-        lat: { min: Infinity, max: -Infinity },
-        lng: { min: Infinity, max: -Infinity },
-    });
-
-      useEffect(() => {
-          // get max/min lat and lng
-          if (coords) {
-              const tempMinMax = {
-                  lat: { min: Infinity, max: -Infinity },
-                  lng: { min: Infinity, max: -Infinity },
-              };
-              for (let coord of coords) {
-                  // console.log(coord);
-                  if (coord.lat > tempMinMax.lat.max) tempMinMax.lat.max = coord.lat;
-                  if (coord.lat < tempMinMax.lat.min) tempMinMax.lat.min = coord.lat;
-                  if (coord.lng > tempMinMax.lng.max) tempMinMax.lng.max = coord.lng;
-                  if (coord.lng < tempMinMax.lng.min) tempMinMax.lng.min = coord.lng;
-              }
-              console.log("tempMinMax", tempMinMax);
-
-              // determine total dimensions of shape by subtracting min from max
-              const width = tempMinMax.lng.max - tempMinMax.lng.min;
-              const height = tempMinMax.lat.max - tempMinMax.lat.min;
-              setMinMax(tempMinMax);
-
-              console.log(width, height);
-
-              //calculate multiplier to convert to new res
-              // hard coded to 500x500 target res right now
-              setMultiplier(width > height  ? 500 / width : 500 / height);
-          }
-      }, [coords]);
-
 
     const draw = (ctx) => {
         // ctx.fillStyle = "#FFFFFF";
@@ -46,6 +10,7 @@ export const Canvas = ({ coords }) => {
         console.log("DRAW");
         let start = {
             x: (coords[0].lng - minMax.lng.min) * multiplier,
+            // y dimension - ((lat - min Lat) * multiplier * (y dimension / original y dimension))
             y: 500 - ((coords[0].lat - minMax.lat.min) * multiplier * 1.36),
         };
         console.log('start', start);
---
+++ src/components/Explorer-SubComponents/StaticMap.js
@@ -0,0 +1,47 @@
+import React, { useCallback, useRef, useState, useEffect } from "react";
+import { GoogleMap, useLoadScript } from "@react-google-maps/api";
+import { libraries, mapContainerStyle, options } from "../../mapConfig/config";
+
+const GOOGLEMAPSKEY = process.env.REACT_APP_GOOGLE_MAPS_KEY;
+
+export const StaticMap = ({ coords, minMax, multiplier }) => {
+    const [center, setCenter] = useState(null);
+    const mapRef = useRef();
+
+    /** loads all the google maps scripts */
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: GOOGLEMAPSKEY,
+        libraries,
+    });
+
+    const onMapLoad = useCallback((map) => {
+        mapRef.current = map;
+    }, []);
+
+    useEffect(() => {
+        if (minMax) {
+            console.log('minMax', minMax);
+            setCenter({
+                lat: (minMax.lat.max - minMax.lat.min) / 2,
+                lng: (minMax.lng.max - minMax.lng.min) / 2,
+            });
+        }
+    }, [minMax]);
+
+    useEffect(() => {
+        console.log ('center', center);
+    }, [center]);
+
+    if (loadError) return "error loading maps";
+    if (!isLoaded) return "LOADING";
+
+    return (
+        <GoogleMap
+            mapContainerStyle={mapContainerStyle}
+            zoom={15} // default zoom level
+            center= {center ? center : { lat: 27.316424, lng: 12.955319 }}
+            options={options}
+            onLoad={onMapLoad}
+        ></ GoogleMap>
+    );
+};
